dd1d84758a8a7a89462ef3d5b050c5b4be15d341
Andreazo Silva <andreazocoisas@gmail.com>
2020-05-26 15:20:00 -0300

    Retornando categorias

--- src/services/ListPostByCategoriesService.js
+++ src/services/ListPostByCategoriesService.js
@@ -3,7 +3,9 @@ import Post from '../schemas/Post';
 class ListPostByCategoriesService {
     async execute({ category_id }) {
 
-        const posts = await Post.find({ categorie: category_id }).populate({
+        const posts = await Post.find({ categorie: category_id })
+        .populate('categorie')
+        .populate({
             path: 'user',
             populate: {
                 path: 'avatar'
