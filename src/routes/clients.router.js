8e88a03fadaf25f38199ebeacde1ff0ca1c51f3d
Andreazo Silva <andreazocoisas@gmail.com>
2020-06-01 20:41:29 -0300

    Criado posts em destaques

--- src/routes/clients.router.js
+++ src/routes/clients.router.js
@@ -1,42 +1,64 @@
 import { Router } from 'express';
 import ListPostsForClientsService from '../services/ListPostsForClientsService';
 import ListPostByCategoriesService from '../services/ListPostByCategoriesService';
+import Post from '../schemas/Post';
 const ClientRouter = Router();
 
 
 ClientRouter.get('/posts', async (request, response) => {
 
-    try {
-        const listPostsForClientsService = new ListPostsForClientsService();
-        const posts = await listPostsForClientsService.execute();
-        return response.json(posts);
+  try {
+    const listPostsForClientsService = new ListPostsForClientsService();
+    const posts = await listPostsForClientsService.execute();
+    return response.json(posts);
 
-    } catch (error) {
-        return response.status(400).json({
-            message: error.message
-        })
-    }
+  } catch (error) {
+    return response.status(400).json({
+      message: error.message
+    })
+  }
 
 })
 
 ClientRouter.get('/posts-by-category', async (request, response) => {
 
-    const { category } = request.query
-  
-    try {
-      const listPostByCategoriesService = new ListPostByCategoriesService();
-  
-      const posts = await listPostByCategoriesService.execute({ category_id: category });
-  
-      return response.json(posts);
-  
-    } catch (error) {
-      return response.status(400).json({
-        message: error.message
+  const { category } = request.query
+
+  try {
+    const listPostByCategoriesService = new ListPostByCategoriesService();
+
+    const posts = await listPostByCategoriesService.execute({ category_id: category });
+
+    return response.json(posts);
+
+  } catch (error) {
+    return response.status(400).json({
+      message: error.message
+    })
+  }
+
+
+})
+
+ClientRouter.get('/posts/highlight', async (request, response) => {
+
+  try {
+    const posts = await Post.find({ highlight: true })
+      .populate('categorie')
+      .populate({
+        path: 'user',
+        populate: {
+          path: 'avatar'
+        }
       })
-    }
-  
-  
-  })
+    return response.json(posts);
+
+  } catch (error) {
+    return response.status(400).json({
+      message: error.message
+    })
+  }
+
+})
 
 export default ClientRouter;
--- src/schemas/Post.js
+++ src/schemas/Post.js
@@ -1,34 +1,34 @@
 import mongoose from 'mongoose';
 
 const PostSchema = mongoose.Schema({
-    file: String,
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    categorie: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Categorie',
-        required: true,
-    },
-    price: Number,
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
-    }
+  file: String,
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  highlight: {
+    type: Boolean,
+    default: false
+  },
+  categorie: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Categorie',
+    required: true,
+  },
+  price: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 }, {
-    timestamps: true,
-    toJSON: {
-      virtuals : true,
-    }
-})
-
-PostSchema.virtual('file_url').get(function() {
-  return `http://localhost:3333/files/${this.file}`
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+  }
 })
 
 export default mongoose.model('Post', PostSchema)
